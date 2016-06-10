a61da1d283cf489ac8e1ec14f6152b6736b1ba60
jzarta <jorge.zarta@knights.ucf.edu>
2016-06-10 08:54:32 -0400

    twitter keys update

--- keys.js
+++ keys.js
@@ -1,8 +1,8 @@
 console.log('this is loaded');
 
 exports.twitterKeys = {
-  consumer_key: '<input here>',
-  consumer_secret: '<input here>',
-  access_token_key: '<input here>',
-  access_token_secret: '<input here>',
+  consumer_key: 'u4od6uYlF8bqxyW9u6WAmtF8p',
+  consumer_secret: 'KPCigcpQP3Q2bKUCq88f0Ryq5N3GemEDSUn3Zxu9GFWxCB4y3X',
+  access_token_key: '60811508-bwzmJVqSUEmawQtUvcRegiW37sCrho0yCzaUEibGc',
+  access_token_secret: 'PbIHyrtAHPcvc3unE7CMM2ox6eOwWRyyMTsVQVlXRYSop',
 }
